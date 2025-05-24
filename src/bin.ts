3181b3451080b237c7d7a23d84e0117aaf0ce378
herb-chan <wujowujo3@gmail.com>
2025-05-24 16:26:57 +0200

    feat: completed the view command and added it to cli

--- src/bin.ts
+++ src/bin.ts
@@ -1,10 +1,16 @@
 import { Command } from 'commander';
+import { viewCommand } from './cmds/view';
 
 const program = new Command();
 
 program
 .name('jsonhero')
-.description('A minimal CLI tool')
+.description('A minimal CLI tool for handling JSON')
 .version('0.0.1');
+
+program
+.command('view <file>')
+.description('Read and pretty-print a JSON file')
+.action(viewCommand);
 
 program.parse();
